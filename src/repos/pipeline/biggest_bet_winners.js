3be0acab32c3d59e3139350186a02ffab72e32e3
Paulo Henrique <paulohr.abreu@gmail.com>
2020-04-27 11:33:05 -0300

    resolve pipeline

--- src/repos/pipeline/biggest_bet_winners.js
+++ src/repos/pipeline/biggest_bet_winners.js
@@ -3,92 +3,110 @@ import mongoose from 'mongoose';
 const pipeline_biggest_bet_winners = (_id, { offset, size }) =>
 [
     {
-        '$match': {
-            '_id': mongoose.Types.ObjectId(_id)
-        }
+      '$match': {
+        '_id': mongoose.Types.ObjectId(_id)
+      }
     }, {
-        '$lookup': {
-            'from': 'games',
-            'localField': 'games',
-            'foreignField': '_id',
-            'as': 'games'
-        }
+      '$lookup': {
+        'from': 'games', 
+        'localField': 'games', 
+        'foreignField': '_id', 
+        'as': 'games'
+      }
     }, {
-        '$unwind': {
-            'path': '$games'
-        }
+      '$unwind': {
+        'path': '$games'
+      }
     }, {
-        '$project': {
-            '_id': false,
-            'app': '$_id',
-            'game': {
-                '_id': '$games._id',
-                'name': '$games.name',
-                'metaName': '$games.metaName',
-                'image_url': '$games.image_url'
-            },
-            'bets': '$games.bets'
-        }
+      '$project': {
+        '_id': false, 
+        'app': '$_id', 
+        'game': {
+          '_id': '$games._id', 
+          'name': '$games.name', 
+          'metaName': '$games.metaName', 
+          'image_url': '$games.image_url'
+        }, 
+        'bets': '$games.bets'
+      }
     }, {
-        '$unwind': {
-            'path': '$bets'
-        }
+      '$unwind': {
+        'path': '$bets'
+      }
     }, {
-        '$lookup': {
-            'from': 'bets',
-            'localField': 'bets',
-            'foreignField': '_id',
-            'as': 'bet'
-        }
+      '$lookup': {
+        'from': 'bets', 
+        'localField': 'bets', 
+        'foreignField': '_id', 
+        'as': 'bet'
+      }
     }, {
-        '$project': {
-            'app': true,
-            'game': true,
-            'bet': {
-                '$arrayElemAt': [
-                    '$bet', 0
-                ]
-            }
+      '$project': {
+        'app': true, 
+        'game': true, 
+        'bet': {
+          '$arrayElemAt': [
+            '$bet', 0
+          ]
         }
+      }
     }, {
-        '$lookup': {
-            'from': 'users',
-            'localField': 'bet.user',
-            'foreignField': '_id',
-            'as': 'bet.user'
-        }
+      '$lookup': {
+        'from': 'users', 
+        'localField': 'bet.user', 
+        'foreignField': '_id', 
+        'as': 'bet.user'
+      }
     }, {
-        '$project': {
-            'app': true,
-            'game': true,
-            'bet': {
-                '_id': '$bet._id',
-                'betAmount': '$bet.betAmount',
-                'winAmount': '$bet.winAmount',
-                'isWon': '$bet.isWon',
-                'timestamp': '$bet.timestamp'
-            },
-            'user': {
-                '$arrayElemAt': [
-                    '$bet.user', 0
-                ]
-            }
-        }
+      '$lookup': {
+        'from': 'currencies', 
+        'localField': 'bet.currency', 
+        'foreignField': '_id', 
+        'as': 'bet.currency'
+      }
     }, {
-        '$project': {
-            'app': true,
-            'game': true,
-            'bet': true,
-            'user': {
-                '_id': '$user._id',
-                'username': '$user.username'
-            }
+      '$project': {
+        'app': true, 
+        'game': true, 
+        'bet': {
+          '_id': '$bet._id', 
+          'betAmount': '$bet.betAmount', 
+          'winAmount': '$bet.winAmount', 
+          'isWon': '$bet.isWon', 
+          'timestamp': '$bet.timestamp'
+        }, 
+        'user': {
+          '$arrayElemAt': [
+            '$bet.user', 0
+          ]
+        }, 
+        'currency': {
+          '$arrayElemAt': [
+            '$bet.currency', 0
+          ]
         }
+      }
+    }, {
+      '$project': {
+        'app': true, 
+        'game': true, 
+        'bet': true, 
+        'user': {
+          '_id': '$user._id', 
+          'username': '$user.username'
+        }, 
+        'currency': {
+          '_id': '$currency._id', 
+          'ticker': '$currency.ticker', 
+          'name': '$currency.name', 
+          'image': '$currency.image'
+        }
+      }
     },
     {
-        '$sort': {
-            'bet.winAmount': -1
-        }
+      '$sort': {
+        'bet.winAmount': -1
+      }
     },
     {
         '$skip': offset
@@ -96,7 +114,6 @@ const pipeline_biggest_bet_winners = (_id, { offset, size }) =>
     {
         '$limit': size
     }
-]
-
+  ];
 
 export default pipeline_biggest_bet_winners;
